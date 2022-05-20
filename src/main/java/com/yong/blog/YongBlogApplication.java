5f51b111054ab6e7550b9b05915f84de85d7f13c
dydwn6565 <55012173+dydwn6565@users.noreply.github.com>
2022-05-20 11:22:32 -0700

    first commit

---
+++ src/main/java/com/yong/blog/YongBlogApplication.java
@@ -0,0 +1,13 @@
+package com.yong.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YongBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YongBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yong/blog/YongBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yong.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YongBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
